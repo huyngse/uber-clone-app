29acdc0369f8bcb9739f5e1aaa17136d610e7ea5
huyngse <ngogiahuy775309@gmail.com>
2024-09-01 15:13:02 +0700

    Sign up screen

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,14 +1,70 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import { icons, images } from "@/constants";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const onSignUpPress = async () => {};
   return (
-    <SafeAreaView>
-      <Text>SignUp</Text>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => {
+              setForm((prev) => ({ ...prev, name: value }));
+            }}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => {
+              setForm((prev) => ({ ...prev, email: value }));
+            }}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your name"
+            icon={icons.lock}
+            value={form.password}
+            secureTextEntry
+            onChangeText={(value) => {
+              setForm((prev) => ({ ...prev, password: value }));
+            }}
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+          <Link href='/sign-in' className="text-center text-general-200 mt-10">
+            <Text>Already have an account? </Text>
+            <Text className="text-primary-500">Log In</Text>
+          </Link>
+        </View>
+      </View>
       <StatusBar style="dark" />
-    </SafeAreaView>
+    </ScrollView>
   );
 };
 
---
+++ components/InputField.tsx
@@ -0,0 +1,52 @@
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Platform,
+  Keyboard,
+} from "react-native";
+import React from "react";
+import { InputFieldProps } from "@/types/type";
+
+const InputField = ({
+  label,
+  labelStyle,
+  icon,
+  secureTextEntry = false,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  className,
+  ...props
+}: InputFieldProps) => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="my-2 w-full">
+          <Text className={`font-JakartaSemiBold mb-3 ${labelStyle}`}>
+            {label}
+          </Text>
+          <View
+            className={`flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
+          >
+            {icon && (
+              <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
+            )}
+            <TextInput
+              className={`rounded-full p-3 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
+              secureTextEntry={secureTextEntry}
+              {...props}
+            ></TextInput>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default InputField;
